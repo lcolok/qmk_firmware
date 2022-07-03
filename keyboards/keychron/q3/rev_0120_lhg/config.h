7c04c4a59e9903d84b39a82d09544a2c09ad301a
lcolok <425311101@qq.com>
2022-07-03 12:31:12 +0800

    加到第十层

--- keyboards/keychron/q3/rev_0120_lhg/config.h
+++ keyboards/keychron/q3/rev_0120_lhg/config.h
@@ -41,7 +41,7 @@
 // 2: Windows
 // 3: Left Handed Gaming
 // 4: Windows Function
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 
 #define RGB_MATRIX_KEYPRESSES
 #define RGB_MATRIX_KEYRELEASES
--- keyboards/keychron/q3/rev_0120_lhg/keymaps/via/keymap.c
+++ keyboards/keychron/q3/rev_0120_lhg/keymaps/via/keymap.c
@@ -22,7 +22,11 @@ enum layers{
     WIN_BASE,
     LEFT_HANDED_GAMING,
     WIN_FN,
-    PLUS_ONE
+    PLUS_ONE,
+    PLUS_TWO,
+    PLUS_THREE,
+    PLUS_FOUR,
+    PLUS_FIVE
 };
 
 enum custom_keycodes {
@@ -62,7 +66,7 @@ static uint8_t mac_keycode[4] = { KC_LOPT, KC_ROPT, KC_LCMD, KC_RCMD };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_87(
-        KC_ESC,             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_SNAP,  KC_SIRI,  RGB_MOD,
+        MT(MOD_LCTL | MOD_LSFT | MOD_LALT | MOD_LGUI,KC_ESC),             KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_SNAP,  KC_SIRI,  RGB_MOD,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,    KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,   KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,             KC_ENT,
@@ -102,6 +106,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO  ,  KC_NO   , KC_NO  ,                                KC_NO  ,                                KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ),
 
     [PLUS_ONE] = LAYOUT_ansi_87(
+        KC_NO  ,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_NO  ,  KC_NO  ,  RGB_TOG,
+        KC_NO  ,  TO(WIN_BASE)   , TO(LEFT_HANDED_GAMING)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        RGB_TOG,  RGB_MOD , RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  NK_TOGG,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        KC_NO  ,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,
+        KC_NO  ,            KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,            KC_NO  ,
+        KC_NO  ,  KC_NO   , KC_NO  ,                                KC_NO  ,                                KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ),
+
+    [PLUS_TWO] = LAYOUT_ansi_87(
+        KC_NO  ,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_NO  ,  KC_NO  ,  RGB_TOG,
+        KC_NO  ,  TO(WIN_BASE)   , TO(LEFT_HANDED_GAMING)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        RGB_TOG,  RGB_MOD , RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  NK_TOGG,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        KC_NO  ,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,
+        KC_NO  ,            KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,            KC_NO  ,
+        KC_NO  ,  KC_NO   , KC_NO  ,                                KC_NO  ,                                KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ),
+
+    [PLUS_THREE] = LAYOUT_ansi_87(
+        KC_NO  ,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_NO  ,  KC_NO  ,  RGB_TOG,
+        KC_NO  ,  TO(WIN_BASE)   , TO(LEFT_HANDED_GAMING)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        RGB_TOG,  RGB_MOD , RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  NK_TOGG,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        KC_NO  ,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,
+        KC_NO  ,            KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,            KC_NO  ,
+        KC_NO  ,  KC_NO   , KC_NO  ,                                KC_NO  ,                                KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ),
+
+    [PLUS_FOUR] = LAYOUT_ansi_87(
+        KC_NO  ,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_NO  ,  KC_NO  ,  RGB_TOG,
+        KC_NO  ,  TO(WIN_BASE)   , TO(LEFT_HANDED_GAMING)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        RGB_TOG,  RGB_MOD , RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  NK_TOGG,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
+        KC_NO  ,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,
+        KC_NO  ,            KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,             KC_NO  ,            KC_NO  ,
+        KC_NO  ,  KC_NO   , KC_NO  ,                                KC_NO  ,                                KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ),
+
+    [PLUS_FIVE] = LAYOUT_ansi_87(
         KC_NO  ,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,  KC_NO  ,  KC_NO  ,  RGB_TOG,
         KC_NO  ,  TO(WIN_BASE)   , TO(LEFT_HANDED_GAMING)  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
         RGB_TOG,  RGB_MOD , RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  NK_TOGG,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,   KC_NO  ,  KC_NO  ,  KC_NO  ,  KC_NO  ,
